184d0eb8a11a778ba2aead92c1072a30dfda80dc
mariobd38 <mbarragan9859@sdsu.edu>
2022-06-27 14:18:43 -0700

    obtaining user registration data

--- social_media/settings.py
+++ social_media/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK="bootstrap5"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- social_media/views.py
+++ social_media/views.py
@@ -13,20 +13,19 @@ def home_view(request):
         account_object = None
         if '@' not in username:
             input_user = Account.objects.filter(username=username)
-            account_object = Account.objects.get(username=username)
         else:  
             input_user = Account.objects.filter(email=email)
-            account_object = Account.objects.get(email=email)
-        
         input_password = input_user.filter(password=password)
         if not input_user or not input_password:
             context = {'error':'Username or password is incorrect'}
             return render(request,"home_view.html",context)
         
         context = {
-            "object":account_object,
+            "object":input_user,
         }
-        return redirect('/user',context=context)
+        return render(request,"user/home_view.html",context=context)
+        # return redirect('/user',context=context)
+        # return redirect('/user')
     return render(request,"home_view.html",{})
 
 def log_in(request):
